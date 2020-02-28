e8a8398fab6308bb3ed6678a9f0686248614d36a
theGamestar <jesseh2003@outlook.com>
2020-02-27 18:33:58 -0800

    reenabled shooting

--- src/main/java/frc/robot/commands/C_PrepShoot.java
+++ src/main/java/frc/robot/commands/C_PrepShoot.java
@@ -14,9 +14,9 @@ public class C_PrepShoot extends CommandBase {
 
   @Override
   public void initialize() {
-    //shooter.updateFromVision(true).setSpinning(true);
-    shooter.testSetTargetRPM(3700);
-    shooter.setSpinning(true);
+    shooter.updateFromVision(true).setSpinning(true);
+    //shooter.testSetTargetRPM(3700);
+    //shooter.setSpinning(true);
     shooter.setHoodFar(true);
   }
 
