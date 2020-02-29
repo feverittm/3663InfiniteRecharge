e89b40f4cbe3f62df79d82f1bca7c045c4729fc9
theGamestar <jesseh2003@outlook.com>
2020-02-28 18:30:00 -0800

    setup the ProjectileArkSimulation with all the varibles

--- src/main/java/frc/robot/utils/ProjectileArkSimulation.java
+++ src/main/java/frc/robot/utils/ProjectileArkSimulation.java
@@ -1,8 +1,17 @@
 package frc.robot.utils;
 
-public static class ProjectileArkSimulation {
+public class ProjectileArkSimulation {
 
-    public ProjectileArkSimulation(double mass,double drag, double ProjectileVolosety){
+    private double D;
+    private double mass;
+    private double Vx;
+    private double Vy;
+    private double angleOfHood;
+    private double hightOfTarget;
+    private double time;
 
+    public ProjectileArkSimulation(double mass, double Vx, double Vy){
+        
     }
+
 }
